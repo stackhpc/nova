bb5a7febc80d78ad8830d8792481006871687ecf
Kashyap Chamarthy <kchamart@redhat.com>
2021-01-28 16:35:10 +0100

    libvirt: Add a workaround to skip compareCPU() on destination
    Nova's use of libvirt's compareCPU() API served its purpose
    over the years, but its design limitations break live migration in
    subtle ways.  For example, the compareCPU() API compares against the
    host physical CPUID.  Some of the features from this CPUID aren not
    exposed by KVM, and then there are some features that KVM emulates that
    are not in the host CPUID.  The latter can cause bogus live migration
    failures.
    
    With QEMU >=2.9 and libvirt >= 4.4.0, libvirt will do the right thing in
    terms of CPU compatibility checks on the destination host during live
    migration.  Nova satisfies these minimum version requirements by a good
    margin.  So, provide a workaround to skip the CPU comparison check on
    the destination host before migrating a guest, and let libvirt handle it
    correctly.  This workaround will be removed once Nova replaces the older
    libvirt APIs with their newer and improved counterparts[1][2].
    
                    - - -
    
    Note that Nova's libvirt driver calls compareCPU() in another method,
    _check_cpu_compatibility(); I did not remove its usage yet.  As it needs
    more careful combing of the code, and then:
    
      - where possible, remove the usage of compareCPU() altogether, and
        rely on libvirt doing the right thing under the hood; or
    
      - where Nova _must_ do the CPU comparison checks, switch to the better
        libvirt CPU APIs -- baselineHypervisorCPU() and
        compareHypervisorCPU() -- that are described here[1].  This is work
        in progress[2].
    
    [1] https://opendev.org/openstack/nova-specs/commit/70811da221035044e27
    [2] https://review.opendev.org/q/topic:bp%252Fcpu-selection-with-hypervisor-consideration
    
    Change-Id: I444991584118a969e9ea04d352821b07ec0ba88d
    Closes-Bug: #1913716
    Signed-off-by: Kashyap Chamarthy <kchamart@redhat.com>
    Signed-off-by: Balazs Gibizer <bgibizer@redhat.com>
    (cherry picked from commit 267a40663cd8d0b94bbc5ebda4ece55a45753b64)

--- nova/conf/workarounds.py
+++ nova/conf/workarounds.py
@@ -290,6 +290,14 @@ Related options:
 * ``compute_driver`` (libvirt)
 * ``[libvirt]/images_type`` (rbd)
 """),
+    cfg.BoolOpt('skip_cpu_compare_on_dest',
+               default=False,
+               help="""
+With the libvirt driver, during live migration, skip comparing guest CPU
+with the destination host. When using QEMU >= 2.9 and libvirt >=
+4.4.0, libvirt will do the correct thing with respect to checking CPU
+compatibility on the destination host during live migration.
+"""),
 ]
 
 
